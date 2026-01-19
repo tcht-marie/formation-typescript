b1c7a09b173545f2f34dd6fcea241877c33ae274
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-19 13:52:26 +0100

    feat: ex07

--- 07_destructuration.js
+++ 07_destructuration.js
@@ -1,53 +1,66 @@
-
 /**
  * utilisez la décomposition pour extraire la premiere et la 2e case du tableau
  * retournez les dans un nouveau tableau
- * 
+ *
  * exemple: [1, 2, 3] => [1, 2]
- * 
+ *
  * astuce: vous pouvez utiliser la décomposition directement dans les arguments de la fonction
- * 
- * contrainte: 
+ *
+ * contrainte:
  *  - interdiction d'utiliser [0] et [1]
  *  - interdiction d'utiliser slice ou splice
  */
 
-const extractFirstTwo = null;
+const extractFirstTwo = (array) => {
+  const [first, second] = array;
+  return [first, second];
+};
 
 /**
  * utilisez la décomposition pour extraire la premiere case du tableau et le reste du tableau
  * retournez uniquement le reste du tableau
- * 
+ *
  * exemple: [1, 2, 3] => [2, 3]
  */
 
-const extractRest = null;
+const extractRest = (array) => {
+  const [first, ...rest] = array;
+  return [...rest];
+};
 
 /**
  * utilisez la décomposition pour extraire le champ "name" de l'objet passé en paramètre
  * retournez le champ "name"
- * 
+ *
  * exemple: {name: "toto", age: 42} => "toto"
- * 
+ *
  * astuce: vous pouvez utiliser la décomposition directement dans les arguments de la fonction
- * 
+ *
  * contrainte:
  * - interdiction d'utiliser l'opérateur "." pour accéder au champ "name"
  */
 
-const extractName = null;
+const extractName = (element) => {
+  const { name, age } = element;
+  return name;
+};
 
 /**
  * utilisez la décomposition pour retourner l'objet utilisateur sans le champ "password"
- * 
+ *
  * exemple: {name: "toto", password: "1234"}
- * 
- * contrainte: 
+ *
+ * contrainte:
  *    - interdiction d'utiliser "delete"
  *    - interdiction d'utiliser l'opérateur "." pour accéder au champ "password"
- * 
+ *
  */
 
-const removePassword = null;
+const removePassword = (element) => {
+  const { name, ...rest } = element;
+  return { name };
+};
+//bichette = { name: "Gotham", password: "12345" };
+//console.log(removePassword(bichette));
 
-module.exports = {extractFirstTwo, extractRest, extractName, removePassword}
+module.exports = { extractFirstTwo, extractRest, extractName, removePassword };
