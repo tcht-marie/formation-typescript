ad762497fb8436d4f81a310b6b6db9b5a8c9c9ba
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-19 14:11:12 +0100

    feat: ex10

--- 10_promise.js
+++ 10_promise.js
@@ -1,9 +1,13 @@
-
 /**
  * Créez une fonction asynchrone qui attend 2 seconde
- * 
+ *
  * utilisez new Promise
  */
 
-const sleep = null;
-module.exports = {sleep};
+const sleep = new Promise(() => {
+  setTimeout(() => {
+    console.log("I'm waiting 2 seconds");
+  }, 2000);
+});
+
+module.exports = { sleep };
