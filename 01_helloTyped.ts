64f2b411198a5859fb2e8d96da893f3cc4fdeb38
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-19 16:31:47 +0100

    feat: ex01

--- 01_helloTyped.ts
+++ 01_helloTyped.ts
@@ -12,7 +12,7 @@
 // TODO: Créer et exporter la fonction greet
 export function greet(name: string): string {
   // À implémenter
-  throw new Error("Not implemented");
+    return `Hello, ${name}!`;
 }
 
 /**
