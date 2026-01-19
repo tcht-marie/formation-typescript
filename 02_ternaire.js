09194b9e33b68c518fe170dc9e5a9e0e96840e86
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-19 10:52:50 +0100

    feat: exercices 2 and 3

--- 02_ternaire.js
+++ 02_ternaire.js
@@ -1,12 +1,11 @@
-
 /**
  * retourne la valeur la plus grande des deux paramètres
- * 
+ *
  * Contraintes:
  *    - utiliser l'opérateur ternaire (if interdit)
  */
-function ternaire(a , b) {
+function ternaire(a, b) {
+  return a > b ? a : b;
 }
-
 
 module.exports = ternaire;
