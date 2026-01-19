715b671129b5b3ff8b9ebee219cbb6dfe0cec42d
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-19 10:56:45 +0100

    feat: ex04

--- 04_arrow.js
+++ 04_arrow.js
@@ -1,23 +1,24 @@
-
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
  * Le return est explicite
- * 
- * contrainte: 
+ *
+ * contrainte:
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (number) => {
+  return number + 1;
+};
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
  * Le return est implicite
- * 
- * contrainte: 
+ *
+ * contrainte:
  *   - le mot clée "function" est interdis
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = (number) => number + 1;
 
-module.exports = {arrow1, arrow2};
+module.exports = { arrow1, arrow2 };
