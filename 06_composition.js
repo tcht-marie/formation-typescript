097677ff50f27afc7e3475d8d941463ab278cfc3
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-19 12:22:36 +0100

    feat: ex06

--- 06_composition.js
+++ 06_composition.js
@@ -1,39 +1,42 @@
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 tableaux passés en paramètres
- * 
+ *
  * ex: [1, 2, 3], [4, 5, 6] => [1, 2, 3, 4, 5, 6]
- * 
- * contrainte: 
+ *
+ * contrainte:
  *  - ne pas utiliser la méthode concat, map, merge, push
  *  - for, foreach, while, do while sont interdits
  */
 
-const concat = null;
+const concat = (array1, array2) => new Array(...array1, ...array2);
 
 /**
  * utiliser l'opérateur de composition ... afin d'ajouter un élément à un tableau
- * 
+ *
  * ex: [1, 2, 3], 4 => [1, 2, 3, 4]
- * 
+ *
  * contrainte:
  * - ne pas utiliser la méthode push
  */
 
-const push = null;
+const push = (array1, element) => new Array(...array1, element);
 
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
- * 
+ *
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
 
-const merge = null;
+const merge = (obj1, obj2) => {
+  const obj3 = { ...obj1, ...obj2 };
+  return obj3;
+};
 
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
- * 
+ *
  * ex: {name: 'toto'}, 'titi' => {name: 'titi'}
- * 
+ *
  * contrainte:
  *  - interdiction d'utiliser l'opérateur d'affectation "="
  */
@@ -41,4 +44,4 @@ const merge = null;
 const setName = null;
 
 // astuce: {...obj} crée une copie de l'objet, c'est un des principes de l'immutabilité et évite les problèmes de référence
-module.exports = {concat, push, merge, setName}
+module.exports = { concat, push, merge, setName };
