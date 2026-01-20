bc8078b9c15e6a2eafa6404f9d9d0588dea1f1ab
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-20 11:27:58 +0100

    feat: ex11

--- 11_objectType.ts
+++ 11_objectType.ts
@@ -17,11 +17,16 @@
 // TODO: Définir le type Car
 
 export type Car = {
-  // À compléter
+  brand: string;
+    model: string;
+    year: number;
 };
 
 // TODO: Implémenter getCarDescription
 
 export function getCarDescription(car: Car): string {
-  throw new Error("Not implemented");
+  return `Voiture : ${car.brand} ${car.model} (${car.year})`;
 }
+
+const car: Car = { brand: "Toyota", model: "Corolla", year: 2020 };
+console.log(getCarDescription(car));
